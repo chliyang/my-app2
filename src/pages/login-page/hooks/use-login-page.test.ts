2575efa6172810b271a1fc8d5c4e9ac71b0652a0
chliyang <chengli.yang@thoughtworks.com>
2022-05-19 00:31:28 +0800

    test: login failed

--- src/pages/login-page/hooks/use-login-page.test.ts
+++ src/pages/login-page/hooks/use-login-page.test.ts
@@ -1,7 +1,7 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import { act } from "react-test-renderer";
 import useLoginPage from "./use-login-page";
-// import * as http from "../../../utils/http/request";
+import http from "../../../utils/http/request";
 
 jest.mock("react-router-dom", () => ({
   useHistory: () => ({
@@ -23,15 +23,22 @@ jest.mock("react-router-dom", () => ({
 //       )
 // );
 
-jest.mock(
-  "../../../utils/http/request",
-  () => () =>
-    Promise.resolve({
-      data: { username: "user name", password: "password" }
-    })
-);
+// jest.mock(
+//   "../../../utils/http/request",
+//   () => () =>
+//     Promise.resolve({
+//       data: { username: "user name", password: "password" }
+//     })
+// );
+
+jest.mock("../../../utils/http/request", () => jest.fn());
 describe("useLoginPage", () => {
   it("should login success", async () => {
+    http.mockImplementation(() =>
+      Promise.resolve({
+        data: { username: "user name", password: "password" }
+      })
+    );
     // mockHttpRequest.getMockImplementation();
     const { result } = renderHook(useLoginPage);
 
@@ -46,6 +53,25 @@ describe("useLoginPage", () => {
     });
     await waitFor(() => {
       expect(result.current.loading).toBe("登录成功");
+    });
+  });
+
+  it("should login failed", async () => {
+    http.mockImplementation(() => Promise.reject(new Error("error")));
+    // mockHttpRequest.getMockImplementation();
+    const { result } = renderHook(useLoginPage);
+
+    act(() => {
+      result.current.setUsername("user name");
+      result.current.setPassword("password");
+      result.current.handleLogin();
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe("正在登录...");
+    });
+    await waitFor(() => {
+      expect(result.current.loading).toBe("登录失败, Error: error");
     });
   });
 });
