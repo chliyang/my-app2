5cb07f1d733a702e86eace1760e8ecc2a7f2c38a
chliyang <chengli.yang@thoughtworks.com>
2022-05-29 22:10:24 +0800

    refactor: extract form configs to map it

--- src/components/login-register-form.tsx
+++ src/components/login-register-form.tsx
@@ -6,6 +6,9 @@ import {
   MailOutlined,
   PhoneOutlined
 } from "@ant-design/icons";
+import { NamePath } from "antd/lib/form/interface";
+import { Rule } from "antd/lib/form";
+import { LiteralUnion } from "antd/lib/_util/type";
 
 export interface ILoginRegisterFormProps {
   loadingMessage: string;
@@ -19,6 +22,19 @@ export interface ILoginRegisterFormProps {
   setPhoneNumber?: React.Dispatch<React.SetStateAction<string>>;
 }
 
+interface IFormItemConfig {
+  alwaysShow: boolean,
+  hasFeedback: boolean,
+  formItemName: string,
+  formItemDependencies: NamePath[],
+  formItemRules: Rule[],
+  testId: string,
+  type: LiteralUnion<'button' | 'checkbox' | 'color' | 'date' | 'datetime-local' | 'email' | 'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week', string>,
+  prefix: React.ReactNode,
+  placeholder: string,
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
+}
+
 const LoginRegisterForm: React.FC<ILoginRegisterFormProps> = ({
   loadingMessage,
   setUsername,
@@ -30,9 +46,104 @@ const LoginRegisterForm: React.FC<ILoginRegisterFormProps> = ({
   setPhoneNumber,
   inputClassName
 }) => {
+  const formItemConfigs: IFormItemConfig[] = [
+    {
+      alwaysShow: true,
+      hasFeedback: false,
+      formItemName: "用户名",
+      formItemDependencies: [],
+      formItemRules: [
+        { required: true, message: "请输入用户名!" },
+        { type: "string", min: 6, message: "用户名长度不小于6" }
+      ],
+      testId: "user-name",
+      type: "",
+      prefix: <UserOutlined className="site-form-item-icon" />,
+      placeholder: "请输入用户名",
+      onChange: (e) => setUsername(e.target.value)
+    },
+    {
+      alwaysShow: true,
+      hasFeedback: false,
+      formItemName: "密码",
+      formItemDependencies: [],
+      formItemRules: [{ required: true, message: "请输入密码!" }],
+      testId: "password",
+      type: "password",
+      prefix: <LockOutlined className="site-form-item-icon" />,
+      placeholder: "请输入密码",
+      onChange: (e) => setPassword(e.target.value)
+    },
+    {
+      alwaysShow: false,
+      hasFeedback: true,
+      formItemName: "confirm",
+      formItemDependencies: ["password"],
+      formItemRules: [
+        {
+          required: true,
+          message: "请二次确认您的密码"
+        },
+        ({ getFieldValue }) => ({
+          validator(_, value) {
+            if (!value || getFieldValue("密码") === value) {
+              return Promise.resolve();
+            }
+            return Promise.reject(new Error("两次输入的密码不一致!"));
+          }
+        })
+      ],
+      testId: "password-confirm",
+      type: "password",
+      prefix: <LockOutlined className="site-form-item-icon" />,
+      placeholder: "请二次确认您的密码"
+    },
+    {
+      alwaysShow: false,
+      hasFeedback: false,
+      formItemName: "email",
+      formItemDependencies: [],
+      formItemRules: [
+        {
+          type: "email",
+          message: "请输入正确的邮箱地址！"
+        },
+        {
+          required: true,
+          message: "请输入邮箱地址！"
+        }
+      ],
+      testId: "email",
+      type: "",
+      prefix: <MailOutlined className="site-form-item-icon" />,
+      placeholder: "请输入邮箱地址",
+      onChange: (e) => setEmail?.(e.target.value)
+    },
+    {
+      alwaysShow: false,
+      hasFeedback: false,
+      formItemName: "phone",
+      formItemDependencies: [],
+      formItemRules: [
+        {
+          required: true,
+          message: "请输入您的电话号码"
+        },
+        {
+          max: 11,
+          message: " 请输入正确的电话号码"
+        }
+      ],
+      testId: "phone",
+      type: "",
+      prefix: <PhoneOutlined className="site-form-item-icon" />,
+      placeholder: "请输入您的电话号码",
+      onChange: (e) => setPhoneNumber?.(e.target.value)
+    }
+  ];
   return (
     <Form
-      name="my-form"
+      name="login-register-form"
       initialValues={{ remember: true }}
       onFinish={handleSubmit}
     >
@@ -41,110 +152,24 @@ const LoginRegisterForm: React.FC<ILoginRegisterFormProps> = ({
           {title}
         </div>
       )}
-      <Form.Item
-        name="用户名"
-        rules={[
-          { required: true, message: "请输入用户名!" },
-          { type: "string", min: 6, message: "用户名长度不小于6" }
-        ]}
-      >
-        <Input
-          data-testid="user-name"
-          className={inputClassName}
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="请输入用户名"
-        />
-      </Form.Item>
-      <Form.Item
-        name="密码"
-        rules={[{ required: true, message: "请输入密码!" }]}
-      >
-        <Input
-          data-testid="password"
-          className={inputClassName}
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="请输入密码"
-        />
-      </Form.Item>
-      {!isLogin && (
-        <Form.Item
-          name="confirm"
-          dependencies={["password"]}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: "请二次确认您的密码"
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue("密码") === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error("两次输入的密码不一致!"));
-              }
-            })
-          ]}
+      {formItemConfigs.map((item) => (
+        (item.alwaysShow || !isLogin) && <Form.Item
+          key={item.formItemName}
+          name={item.formItemName}
+          dependencies={item.formItemDependencies}
+          rules={item.formItemRules}
+          hasFeedback={item.hasFeedback}
         >
           <Input
-            data-testid="password-confirm"
+            data-testid={item.testId}
+            type={item.type}
             className={inputClassName}
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="请二次确认您的密码"
+            prefix={item.prefix}
+            placeholder={item.placeholder}
+            onChange={item.onChange}
           />
         </Form.Item>
-      )}
-
-      {!isLogin && (
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              type: "email",
-              message: "请输入正确的邮箱地址！"
-            },
-            {
-              required: true,
-              message: "请输入邮箱地址！"
-            }
-          ]}
-        >
-          <Input
-            data-testid="email"
-            className={inputClassName}
-            prefix={<MailOutlined className="site-form-item-icon" />}
-            placeholder="请输入邮箱地址"
-            onChange={(e) => setEmail?.(e.target.value)}
-          />
-        </Form.Item>
-      )}
-      {!isLogin && (
-        <Form.Item
-          name="phone"
-          rules={[
-            {
-              required: true,
-              message: "请输入您的电话号码"
-            },
-            {
-              max: 11,
-              message: " 请输入正确的电话号码"
-            }
-          ]}
-        >
-          <Input
-            data-testid="phone"
-            className={inputClassName}
-            prefix={<PhoneOutlined className="site-form-item-icon" />}
-            placeholder="请输入您的电话号码"
-            onChange={(e) => setPhoneNumber?.(e.target.value)}
-          />
-        </Form.Item>
-      )}
+      ))}
       <Form.Item>
         <Button
           data-testid="submit-button"
