bbb935a95d9d9faf259fd82158d53632b640c9e6
chliyang <chengli.yang@thoughtworks.com>
2022-06-01 22:12:11 +0800

    style: add min width for login form

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -5,7 +5,7 @@ import useLoginPage from "./hooks/use-login-page";
 const LoginPage: React.FC = () => {
   const { loading, handleLogin } = useLoginPage();
   return (
-    <div data-testid="login-form" className="box-border h-1/3 w-1/2 lg:w-1/4 bg-white shadow-lg p-10 text-lg">
+    <div data-testid="login-form" className="box- min-w-max h-1/3 w-1/2 lg:w-1/4 bg-white shadow-lg p-10 text-lg">
       <LoginRegisterForm
         inputClassName="h-16 w-full text-lg"
         loadingMessage={loading}
