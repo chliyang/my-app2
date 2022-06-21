78f9c7e9f45b02c40e8effc60217d20c435335b1
chliyang <chengli.yang@thoughtworks.com>
2022-06-21 13:43:32 +0800

    feat: custom tailwind styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import Root from "./routes/root";
 import reportWebVitals from './reportWebVitals';
 import "./index.css";
 import "./mock";
+import "./css/tailwind.output.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
