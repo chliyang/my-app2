ec9e40c9ec31b37da2bb899082de4432e6461af0
chliyang <chengli.yang@thoughtworks.com>
2022-07-10 22:19:58 +0800

    feat: extract product list

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,8 +1,8 @@
-import { PictureOutlined } from "@ant-design/icons";
 import { Input } from "antd";
 import React from "react";
 import HeaderList from "./components/header-list";
 import Menu from "./components/menu";
+import ProductList from "./components/product-list";
 
 const FuncSection: React.FC<{ label: string }> = ({ label, children }) => {
   return (
@@ -44,85 +44,7 @@ const HomePage: React.FC = () => {
           </FuncSection>
 
           <FuncSection label="商品列表">
-            <div className="grid grid-cols-1 divide-y-1 mx-8 pb-8">
-              <div className="flex h-20 border-bottom-default items-center ">
-                <PictureOutlined className="flex text-5xl items-center" />
-                <div className="flex-1 ml-4 font-medium">
-                  <div>商品名称</div>
-                  <div>商品价格：（元）</div>
-                </div>
-                <div className="flex flex-col">
-                  <button>详情</button>
-                  <button>收藏</button>
-                </div>
-              </div>
-              <div className="flex h-20 border-bottom-default items-center ">
-                <PictureOutlined className="flex text-5xl items-center" />
-                <div className="flex-1 ml-4 font-medium">
-                  <div>商品名称</div>
-                  <div>商品价格：（元）</div>
-                </div>
-                <div className="flex flex-col">
-                  <button>详情</button>
-                  <button>收藏</button>
-                </div>
-              </div>
-              <div className="flex h-20 border-bottom-default items-center ">
-                <PictureOutlined className="flex text-5xl items-center" />
-                <div className="flex-1 ml-4 font-medium">
-                  <div>商品名称</div>
-                  <div>商品价格：（元）</div>
-                </div>
-                <div className="flex flex-col">
-                  <button>详情</button>
-                  <button>收藏</button>
-                </div>
-              </div>
-              <div className="flex h-20 border-bottom-default items-center ">
-                <PictureOutlined className="flex text-5xl items-center" />
-                <div className="flex-1 ml-4 font-medium">
-                  <div>商品名称</div>
-                  <div>商品价格：（元）</div>
-                </div>
-                <div className="flex flex-col">
-                  <button>详情</button>
-                  <button>收藏</button>
-                </div>
-              </div>
-              <div className="flex h-20 border-bottom-default items-center ">
-                <PictureOutlined className="flex text-5xl items-center" />
-                <div className="flex-1 ml-4 font-medium">
-                  <div>商品名称</div>
-                  <div>商品价格：（元）</div>
-                </div>
-                <div className="flex flex-col">
-                  <button>详情</button>
-                  <button>收藏</button>
-                </div>
-              </div>
-              <div className="flex h-20 border-bottom-default items-center ">
-                <PictureOutlined className="flex text-5xl items-center" />
-                <div className="flex-1 ml-4 font-medium">
-                  <div>商品名称</div>
-                  <div>商品价格：（元）</div>
-                </div>
-                <div className="flex flex-col">
-                  <button>详情</button>
-                  <button>收藏</button>
-                </div>
-              </div>
-              <div className="flex h-20 border-bottom-default items-center ">
-                <PictureOutlined className="flex text-5xl items-center" />
-                <div className="flex-1 ml-4 font-medium">
-                  <div>商品名称</div>
-                  <div>商品价格：（元）</div>
-                </div>
-                <div className="flex flex-col">
-                  <button>详情</button>
-                  <button>收藏</button>
-                </div>
-              </div>
-            </div>
+            <ProductList />
           </FuncSection>
         </div>
         <div className="h-full w-1/4 border-solid border-gray-300">
