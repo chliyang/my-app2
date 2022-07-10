9ab34733fd77509c9c43883673b17779d34496c7
chliyang <chengli.yang@thoughtworks.com>
2022-07-11 00:10:04 +0800

    feat: extract side section of main

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -2,6 +2,7 @@ import { Input } from "antd";
 import React from "react";
 import HeaderList from "./components/header-list";
 import Menu from "./components/menu";
+import SideSection from "./components/side-section";
 import ProductCLasses from "./components/product-categories";
 import ProductList from "./components/product-list";
 
@@ -40,75 +41,7 @@ const HomePage: React.FC = () => {
             <ProductList />
           </FuncSection>
         </div>
-        <div className="h-full w-1/4 border-solid border-gray-300">
-          <div className="text-center bg-blue-50">
-            <div className="bg-blue-200 text-base font-medium tracking-widest p-2.5">网站简介</div>
-            <div className="p-4 text-left">
-              您可以发布任何不需要的，但是可以重复使用的物品（除限制物品以外）到本平台进行售卖，也可在本网站搜索您想要购买的物品进行购买。
-            </div>
-            <button className="bg-blue-300 border-blue-300  mb-6">如何使用本网站</button>
-          </div>
-          <div className="text-center bg-blue-50">
-            <div className="bg-blue-200 text-base font-medium tracking-widest p-2.5">最新消息</div>
-            <div className="p-4 text-left">
-              您可以发布任何不需要的，但是可以重复使用的物品（除限制物品以外）到本平台进行售卖，也可在本网站搜索您想要购买的物品进行购买。
-            </div>
-            <button className="bg-blue-300 border-blue-300 mb-6">更多消息</button>
-          </div>
-          <div className="text-center bg-blue-50">
-            <div className="bg-blue-200 text-base font-medium tracking-widest p-2.5">物品推广</div>
-            <div className="p-4 text-left">
-              <div className="p-1 mb-3 rounded-md drop-shadow-md bg-white">
-                <div className="w-full flex items-center">
-                  <div className="h-20 w-20 border-solid border-gray-200 p-1">
-                    <img src={require("../../assets/light.jpg")} alt="Light" className="w-full" />
-                  </div>
-                  <div className="flex-1 overflow-hidden ml-2.5">
-                    <div className="truncate">物品名称sdfsdfsdfsdfsdfsdfsdf</div>
-                    <div className="truncate">物品价格</div>
-                    <div className="truncate">物品分类</div>
-                  </div>
-                </div>
-                <div className="grid grid-cols-2 justify-items-center mt-2.5">
-                  <button className="bg-blue-300 border-blue-300">详情</button>
-                  <button className="bg-blue-300 border-blue-300">收藏</button>
-                </div>
-              </div>
-              <div className="p-1 mb-3 rounded-md drop-shadow-md bg-white">
-                <div className="w-full flex items-center">
-                  <div className="h-20 w-20 border-solid border-gray-200 p-1">
-                    <img src={require("../../assets/light.jpg")} alt="Light" className="w-full" />
-                  </div>
-                  <div className="flex-1 overflow-hidden ml-2.5">
-                    <div className="truncate">物品名称sdfsdfsdfsdfsdfsdfsdf</div>
-                    <div className="truncate">物品价格</div>
-                    <div className="truncate">物品分类</div>
-                  </div>
-                </div>
-                <div className="grid grid-cols-2 justify-items-center mt-2.5">
-                  <button className="bg-blue-300 border-blue-300">详情</button>
-                  <button className="bg-blue-300 border-blue-300">收藏</button>
-                </div>
-              </div>
-              <div className="p-1 mb-3 rounded-md drop-shadow-md bg-white">
-                <div className="w-full flex items-center">
-                  <div className="h-20 w-20 border-solid border-gray-200 p-1">
-                    <img src={require("../../assets/light.jpg")} alt="Light" className="w-full" />
-                  </div>
-                  <div className="flex-1 overflow-hidden ml-2.5">
-                    <div className="truncate">物品名称sdfsdfsdfsdfsdfsdfsdf</div>
-                    <div className="truncate">物品价格</div>
-                    <div className="truncate">物品分类</div>
-                  </div>
-                </div>
-                <div className="grid grid-cols-2 justify-items-center mt-2.5">
-                  <button className="bg-blue-300 border-blue-300">详情</button>
-                  <button className="bg-blue-300 border-blue-300">收藏</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SideSection />
       </main>
     </div>
   );
