3ae0f9e415224b7982ca84410faaaaac8a83d51b
chliyang <chengli.yang@thoughtworks.com>
2022-07-26 00:06:31 +0800

    feat: add translation for tile

--- src/pages/home-page/components/product-list-item.tsx
+++ src/pages/home-page/components/product-list-item.tsx
@@ -1,5 +1,6 @@
 import { InfoCircleOutlined, StarOutlined } from "@ant-design/icons";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export interface IProductListItemProps {
   productImg: string,
@@ -8,14 +9,15 @@ export interface IProductListItemProps {
 }
 
 const ProductListItem: React.FC<IProductListItemProps> = ({ productImg, productName, productPrice }) => {
+  const { t } = useTranslation();
   return (
     <div className="flex h-20 border-bottom-default items-center">
       <div className="h-12 w-12 border-solid border-gray-200 p-1">
-        <img src={require("../../../assets/" + productImg)} alt="商品图片" className="w-full" />
+        <img src={require("../../../assets/" + productImg)} alt={t("home.tile_image")} className="w-full" />
       </div>
       <div className="flex-1 ml-4 font-medium">
         <div>{productName}</div>
-        <div>{`商品价格：${productPrice}（元）`}</div>
+        <div>{t("home.tile_description", { price: productPrice })}</div>
       </div>
       <div className="flex space-y-1 flex-row">
         <button className="bg-white border-none items-center text-lg"><StarOutlined className="mt-1 mx-2" /></button>
