27131fe789676a0e8ce6457a50f9be71bd288834
chliyang <chengli.yang@thoughtworks.com>
2022-07-29 19:01:00 +0800

    feat: checkbox for product category

--- src/pages/home-page/components/product-categories.tsx
+++ src/pages/home-page/components/product-categories.tsx
@@ -1,19 +1,36 @@
 import React from "react";
+import { Checkbox, Col, Row } from 'antd';
+import type { CheckboxValueType } from 'antd/es/checkbox/Group';
+
+const onChange = (checkedValues: CheckboxValueType[]) => {
+  console.log('checked = ', checkedValues);
+};
 
 const ProductCLasses: React.FC = () => {
   // TODO: 加入数据后记得给组件加测试
   return (
-    <div className="m-8 grid grid-cols-4 gap-10 justify-items-center">
-      <div>食品</div>
-      <div>服装</div>
-      <div>家具</div>
-      <div>电器</div>
-      <div>化妆品</div>
-      <div>保健品</div>
-      <div>电子产品</div>
-      <div>清洁用具</div>
-      <div>洗浴用品</div>
-    </div>
+    <Checkbox.Group className="w-full pl-16 text-4xl" onChange={onChange}>
+      <Row>
+        <Col span={6}>
+          <Checkbox value="food">food</Checkbox>
+        </Col>
+        <Col span={6}>
+          <Checkbox value="clothes">clothes</Checkbox>
+        </Col>
+        <Col span={6}>
+          <Checkbox value="furniture">furniture</Checkbox>
+        </Col>
+        <Col span={6}>
+          <Checkbox value="electric">electric</Checkbox>
+        </Col>
+        <Col span={6}>
+          <Checkbox value="toiletries">toiletries</Checkbox>
+        </Col>
+        <Col span={6}>
+          <Checkbox value="cleaning">cleaning</Checkbox>
+        </Col>
+      </Row>
+    </Checkbox.Group>
   );
 };
 
