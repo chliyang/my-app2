99f3189d23d9bab23962ba9801e31335a06ce9f2
chliyang <chengli.yang@thoughtworks.com>
2022-08-10 00:06:25 +0800

    feat: add favorite toast

--- src/pages/home-page/components/favorite-button.tsx
+++ src/pages/home-page/components/favorite-button.tsx
@@ -1,5 +1,7 @@
 import { StarFilled, StarOutlined } from "@ant-design/icons";
+import { message, Spin } from "antd";
 import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { setProducts } from "../../../actions/product-actions/dispatcher";
 import {
   fetchProduct,
@@ -17,25 +19,40 @@ const FavoriteButton: React.FC<{
   const [isFavorite, setIsFavorite] = useState<boolean>(
     currentProduct.isFavorite
   );
+  const [isDisable, setIsDisable] = useState<boolean>(false);
+  const { t } = useTranslation();
   const { dispatch } = useProductContext();
 
   const handleClick = async () => {
+    setIsDisable(true);
     await updateProduct(currentProduct.productId, {
       isFavorite: !isFavorite
-    });
+    })
+      .then(() => {
+        setIsDisable(false);
+        setIsFavorite(!isFavorite);
+        message.success(t("detail.toast_action_success"), 2);
+      })
+      .catch(() => {
+        setIsDisable(false);
+        message.error(t("detail.toast_action_failed"), 2);
+      });
+
     fetchProduct.then((res) => setProducts(dispatch, res.data));
-    setIsFavorite(!isFavorite);
   };
   return (
     <button
       className={`bg-white border-none items-center text-lg ${className}`}
+      disabled={isDisable}
       onClick={handleClick}
     >
-      {isFavorite ? (
-        <StarFilled className="mt-2 mx-2" />
-      ) : (
-        <StarOutlined className="mt-2 mx-2" />
-      )}
+      {isDisable && <Spin className="mt-2 mx-2" />}
+      {!isDisable &&
+        (isFavorite ? (
+          <StarFilled className="mt-2 mx-2" />
+        ) : (
+          <StarOutlined className="mt-2 mx-2" />
+        ))}
     </button>
   );
 };
