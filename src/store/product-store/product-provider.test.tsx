297982662b635536d3bd26bffa72ce4d05a0486c
chliyang <chengli.yang@thoughtworks.com>
2022-08-11 22:58:10 +0800

    test: product provider test

---
+++ src/store/product-store/product-provider.test.tsx
@@ -0,0 +1,45 @@
+import * as productActions from "../../actions/product-actions/product";
+import ProductProvider, {
+  IProduct,
+  useProductContext
+} from "./product-provider";
+import { render, screen, waitFor } from "@testing-library/react";
+
+describe("#ProductProvider", () => {
+  it("should render children component with provider values", async () => {
+    // fetchProduct.mockResolvedValue([{ id: 1, name: "Li" }]);
+    const mockData: IProduct[] = [
+      {
+        productId: "1",
+        productName: "apple",
+        isFavorite: true,
+        productPrice: "200",
+        productImg: "light.jpg",
+        category: "food",
+        createdAt: "2022-2-22",
+        createdBy: "wang",
+        description: "This is an apple"
+      }
+    ];
+    jest.spyOn(productActions, "fetchProduct").mockImplementation(() =>
+      Promise.resolve({
+        data: mockData
+      })
+    );
+
+    const Child = () => {
+      const { state } = useProductContext();
+      return (
+        <div>{state.products.length && state.products[0].productName}</div>
+      );
+    };
+
+    render(
+      <ProductProvider>
+        <Child />
+      </ProductProvider>
+    );
+
+    await waitFor(() => expect(screen.getByText("apple")).toBeInTheDocument());
+  });
+});
